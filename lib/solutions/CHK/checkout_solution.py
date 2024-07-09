75760e8517518ea9252bd62302ae8eee8ee94814
sbx_user1051 <sbx_user1051@169.254.115.53>
2024-07-09 16:44:19 +0000

    Tue Jul 09 16:44:19 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,35 +1,35 @@
 from collections import Counter
 
-# +------+-------+------------------------+
-# | Item | Price | Special offers         |
-# +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
-# | C    | 20    |                        |
-# | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# | F    | 10    | 2F get one F free      |
-# | G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | I    | 35    |                        |
-# | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
-# | L    | 90    |                        |
-# | M    | 15    |                        |
-# | N    | 40    | 3N get one M free      |
-# | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | R    | 50    | 3R get one Q free      |
-# | S    | 30    |                        |
-# | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
-# | W    | 20    |                        |
-# | X    | 90    |                        |
-# | Y    | 10    |                        |
-# | Z    | 50    |                        |
-# +------+-------+------------------------+
+# +------+-------+---------------------------------+
+# | Item | Price | Special offers                  |
+# +------+-------+---------------------------------+
+# | A    | 50    | 3A for 130, 5A for 200          |
+# | B    | 30    | 2B for 45                       |
+# | C    | 20    |                                 |
+# | D    | 15    |                                 |
+# | E    | 40    | 2E get one B free               |
+# | F    | 10    | 2F get one F free               |
+# | G    | 20    |                                 |
+# | H    | 10    | 5H for 45, 10H for 80           |
+# | I    | 35    |                                 |
+# | J    | 60    |                                 |
+# | K    | 70    | 2K for 120                      |
+# | L    | 90    |                                 |
+# | M    | 15    |                                 |
+# | N    | 40    | 3N get one M free               |
+# | O    | 10    |                                 |
+# | P    | 50    | 5P for 200                      |
+# | Q    | 30    | 3Q for 80                       |
+# | R    | 50    | 3R get one Q free               |
+# | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | U    | 40    | 3U get one U free               |
+# | V    | 50    | 2V for 90, 3V for 130           |
+# | W    | 20    |                                 |
+# | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# +------+-------+---------------------------------+
 
 SKUS = {
     "A": 50,
@@ -99,6 +99,7 @@ def apply_specials_discount(item_counts):
                 item_counts[product] -= items_used
                 if condition:
                     item_counts[condition] -= 1
+
     return discount_total
 
 
@@ -115,7 +116,8 @@ def checkout(skus):
 
         total_price += SKUS[sku]
 
-    discounts = apply_specials_discount(Counter(skus))
+    discount = apply_specials_discount(Counter(skus))
 
-    return total_price - discounts
+    return total_price - discount
+
 
