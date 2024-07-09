eb37657bafe99e3cad419aa8f182e7f41695c69d
sbx_user1051 <sbx_user1051@169.254.115.53>
2024-07-09 17:23:22 +0000

    Tue Jul 09 17:23:22 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -49,3 +49,8 @@ class TestCheckout:
     @pytest.mark.parametrize("basket,value", [("XYZ", 45), ("XYZXYZ", 90), ("XYZST", 82), ("YXYZST", 90), ("YXXYZST", 107)])
     def test_group_discount(self, basket, value):
         assert checkout_solution.checkout(basket) == value
+
+    @pytest.mark.parametrize("basket,value",
+                             [("K", 70), ("ABCDEFGHIJKLMNOPQRSTUVW", 795)])
+    def test_failures(self, basket, value):
+        assert checkout_solution.checkout(basket) == value
