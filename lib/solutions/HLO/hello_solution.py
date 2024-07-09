e5943b3a25a37712d2237404e261ef9ee35b2a3f
sbx_user1051 <sbx_user1051@169.254.115.53>
2024-07-09 15:16:17 +0000

    Tue Jul 09 15:16:17 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    if friend_name:
+        return f"Hello, {friend_name}!"
     return "Hello, World!"
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -1,6 +1,9 @@
 from solutions.HLO import hello_solution
 
 
-class TestSum():
-    def test_sum(self):
-        assert hello_solution.hello("Alan") == "Hello, World!"
+class TestHello:
+    def test_hello_name(self):
+        assert hello_solution.hello("Alan") == "Hello, Alan!"
+
+    def test_hello_no_name(self):
+        assert hello_solution.hello("") == "Hello, World!"
